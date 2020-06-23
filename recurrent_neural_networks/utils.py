ec2671dab342c8e57a8005f134aca7343c725067
sgalella <santi.galella@gmail.com>
2020-06-23 18:54:31 +0200

    Implement get_state_vowel

--- recurrent_neural_networks/utils.py
+++ recurrent_neural_networks/utils.py
@@ -1,12 +1,21 @@
 import numpy as np
 
+XOR_GATE = np.array([[1, 0, 1], [0, 0, 0], [0, 1, 1], [1, 1, 0]])
+
+STATE_VOWELS = {
+    'A': [1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1],
+    'E': [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1],
+    'I': [0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0],
+    'O': [1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+    'U': [1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+}
+
 
 def create_xor_data(length):
     # Create inputs
-    xor = np.array([[1, 0, 1], [0, 0, 0], [0, 1, 1], [1, 1, 0]])
     xor_inputs = np.zeros((length, 12), dtype=np.int)
     for row in range(length):
-        xor_inputs[row, :] = np.random.permutation(xor).flatten()
+        xor_inputs[row, :] = np.random.permutation(XOR_GATE).flatten()
     xor_inputs = xor_inputs.flatten()
 
     # Create outputs
@@ -14,3 +23,7 @@ def create_xor_data(length):
     xor_outputs[0:-1] = xor_inputs[1:]
     xor_outputs[-1] = xor_inputs[0]
     return xor_inputs, xor_outputs
+
+
+def get_state_vowel(letter):
+    return STATE_VOWELS[letter]
